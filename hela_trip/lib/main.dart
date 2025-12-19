fbff185168e5be45a014b2bd510a80cc234cf5ad
sahan <edu.sahan@gmail.com>
2025-12-19 22:20:51 +0530

    Implement Firebase initialization and create Firestore service class

--- hela_trip/lib/main.dart
+++ hela_trip/lib/main.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:developer';
-import 'firebase_options.dart';
 
-void main() async {
+import 'package:hela_trip/firebase/firebase_init.dart';
+import 'package:hela_trip/services/firestore_services.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+  await initializeFirebase();
   runApp(const MyApp());
 }
 
@@ -22,29 +18,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Firestore Test App',
-      home: const FirestoreTestPage(),
+      home: FirestoreTestPage(),
     );
   }
 }
 
 class FirestoreTestPage extends StatelessWidget {
-  const FirestoreTestPage({super.key});
+  FirestoreTestPage({super.key});
+
+  // ✅ Create service instance
+  final FirebaseService _firebaseService = FirebaseService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Firestore Test'),
-        centerTitle: true,
-      ),
+      appBar: AppBar(title: const Text('Firestore Test'), centerTitle: true),
       body: Center(
         child: ElevatedButton(
           onPressed: () async {
             try {
-              await FirebaseFirestore.instance.collection('test').add({
-                'message': 'Hello Firebase',
-                'time': DateTime.now(),
-              });
+              await _firebaseService.addMessage();
               log('Data sent to Firestore successfully!');
               ScaffoldMessenger.of(context).showSnackBar(
                 const SnackBar(content: Text('Data sent to Firestore ✅')),

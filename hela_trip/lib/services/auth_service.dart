b3208a86fec1507fcfd5d7edf371216c55379ad0
sahan <edu.sahan@gmail.com>
2026-01-09 00:46:33 +0530

    Add UserProfile page and update GoogleAuthService for user navigation

--- hela_trip/lib/services/auth_service.dart
+++ hela_trip/lib/services/auth_service.dart
@@ -1,53 +1,60 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class GoogleAuthService{
-
+class GoogleAuthService {
   // Firebase service that manages authentication in your app
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
   // Open the Google account selection screen
-  final GoogleSignIn _googleSignIn = GoogleSignIn(clientId: "165574279956-di8m647fn6i1iha3vru3t84dqnts5tsm.apps.googleusercontent.com");
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    clientId:
+        "165574279956-di8m647fn6i1iha3vru3t84dqnts5tsm.apps.googleusercontent.com",
+  );
 
-  Future<User?> SignInWithGoogle ()async{
-    
-    try{
+  Future<User?> signInWithGoogle() async {
+    try {
       final GoogleSignInAccount? googleAccount = await _googleSignIn.signIn();
 
-    // if not select any account return null
-    if(googleAccount == null) return null;
+      // if not select any account return null
+      if (googleAccount == null) return null;
 
-    // Gets access token + ID token from Google
-    final GoogleSignInAuthentication googleAuth = await googleAccount.authentication;
+      // Gets access token + ID token from Google
+      final GoogleSignInAuthentication googleAuth =
+          await googleAccount.authentication;
 
-    // Converts Google tokens into Firebase credentials
-    final credentials = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    ); 
+      // Converts Google tokens into Firebase credentials
+      final credentials = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
 
-    // Firebase verifies the credential and log user into app
-    final UserCredential userCredential = await firebaseAuth.signInWithCredential(credentials);
+      // Firebase verifies the credential and log user into app
+      final UserCredential userCredential = await firebaseAuth
+          .signInWithCredential(credentials);
 
-    // Extracts the authenticated Firebase user from the login result
-    final User user = userCredential.user!;
+      // Extracts the authenticated Firebase user from the login result
+      final User user = userCredential.user!;
 
-    await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
-      'uid': user.uid,
-      'name': user.displayName,
-      'email': user.email,
-      'photoUrl': user.photoURL,
-      'createdAt': FieldValue.serverTimestamp(),
-    });SetOptions(merge: true);
+      await FirebaseFirestore.instance
+          .collection('reg-users')
+          .doc(user.uid)
+          .set(
+          {
+            'uid': user.uid,
+            'name': user.displayName,
+            'email': user.email,
+            'photoUrl': user.photoURL,
+            'createdAt': FieldValue.serverTimestamp(),
+          }, SetOptions(merge: true)
+          );
 
-    return user;
-    }
-    catch(e){
-      print("Google Sign-In Error: $e");
+      return user;
+
+    } catch (e) {
+      debugPrint("Google Sign-In Error: $e");
       return null;
     }
   }
-
-
 }
